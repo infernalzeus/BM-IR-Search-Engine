8b3c188491eb8e759a776dda5525369621d8dc41
Sarthak Agarwal <sarthakagarwal152@gmail.com>
2023-04-11 05:49:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ mkdir data/index/ data/$DATASET
 curl -o data/$DATASET.zip -L https://mind201910small.blob.core.windows.net/release/$DATASET.zip 
 unzip data/$DATASET.zip -d data/$DATASET
 rm data/$DATASET.zip
+pip install -r requirements.txt
